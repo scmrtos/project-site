28c7dbaffa5d84aafddc130688735f30464691cb
hz <harry.zhurov@gmail.com>
2026-01-16 23:38:22 +0700

    add sans font type specification

--- script/md_pdf.py
+++ script/md_pdf.py
@@ -2,6 +2,7 @@
 from pathlib import Path
 from utils   import *
 
+#------------------------------------------------------------------------------
 pd_opt = [
     '--lua-filter=script/br2newline.lua',
     '--lua-filter=script/md2admon.lua',
@@ -23,14 +24,23 @@ pd_opt = [
     '--no-highlight',
     '-V', 'listings=false',
     '-V', 'codeBlockSurroundings=minted',
+
     '-V', 'mainfont=titilliumwebrusbydaymarius',
     '-V', 'mainfontoptions=Path=docs/font/TitilliumWebRUS/',
     '-V', 'mainfontoptions=Extension=.ttf',
     '-V', 'mainfontoptions=UprightFont=*_rg',
     '-V', 'mainfontoptions=BoldFont=*_bd',
+
     '-V', 'monofont=TerminusTTF-Bold-4.39.ttf',
     '-V', 'monofontoptions=docs/font/',
     '-V', 'monofontoptions=Scale=0.9',
+
+    '-V', 'sansfont=titilliumwebrusbydaymarius',
+    '-V', 'sansfontoptions=Path=docs/font/TitilliumWebRUS/',
+    '-V', 'sansfontoptions=Extension=.ttf',
+    '-V', 'sansfontoptions=UprightFont=*_rg',
+    '-V', 'sansfontoptions=BoldFont=*_bd',
+
     '-V', 'block-headings=true',
     '-V', 'header-includes=\\widowpenalty=10000 \\clubpenalty=10000 \\RedeclareSectionCommand[beforeskip=1.8ex plus 0.5ex minus 0.2ex,afterskip=0.8ex plus 0.2ex minus 0.1ex,font=\\large\\bfseries]{paragraph}\\RedeclareSectionCommand[beforeskip=1.4ex plus 0.4ex minus 0.2ex,afterskip=0.6ex plus 0.1ex minus 0.1ex,font=\\normalsize\\bfseries\\itshape]{subparagraph}',
     '-M', 'secnumdepth=0',
